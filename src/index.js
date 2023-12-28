4f5ce54d825a5a5d7547b7451a654cd5d7beab76
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-28 14:00:32 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+  let form=document.querySelector('form')
+  form.addEventListener('submit', (e)=>{
+e.preventDefault()
+const input=document.querySelector('input#searchByID')
+fetch(`http://localhost:3001/movies/${input.value}`)
+.then(res=>res.json())
+.then(movies=>{
+    let title=document.querySelector("section#movieDetails h4")
+    let summary=document.querySelector("section#movieDetails p")
+title.innerText=movies.title;
+summary.innerText=movies.summary;
+})
+})
 }
-
 document.addEventListener('DOMContentLoaded', init);
